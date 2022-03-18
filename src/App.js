ed97e6cbc1e9a47a43aeae06b0b1b0b2eafee3bd
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-03-18 19:01:32 +0200

    add friendlist

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Profile from "./components/Profile/Profile";
 import user from "./DataBase/user.json";
 import Statistics from "./components/Statistics/Statistics";
 import data from "./DataBase/data.json";
+import FriendList from "./components/FriendList/FriendList";
+import friends from "./DataBase/friends.json";
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
       />
       <Statistics title="Upload stats" stats={data} />;
       {/* <Statistics stats={data} />; */}
+      <FriendList friends={friends} />,
     </div>
   );
 }
---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+export default function FriendList({ friends }) {
+  return (
+    <ul className="friend-list">
+      {friends.map((friend) => (
+        <li className="item" key={friend.id}>
+          <span
+            style={{ backgroundColor: friend.isOnline ? "green" : "red" }}
+          ></span>
+          <img className="avatar" src={friend.avatar} width="50" />
+          <span className="name">{friend.name}</span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.array,
+};
