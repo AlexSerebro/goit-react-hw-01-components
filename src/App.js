e1982ac5ae89591b6f94cb521d5c9822f4565ce2
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-03-19 12:54:48 +0200

    add container

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import Statistics from "./components/Statistics/Statistics";
 import data from "./DataBase/data.json";
 import FriendList from "./components/FriendList/FriendList";
 import friends from "./DataBase/friends.json";
+import Container from "./components/Container/Container";
 
 function App() {
   return (
     <div className="App">
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        followers={user.stats.followers}
-        views={user.stats.views}
-        likes={user.stats.likes}
-      />
-      <Statistics title="Upload stats" stats={data} />;
-      {/* <Statistics stats={data} />; */}
-      <FriendList friends={friends} />,
+      <Container>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          followers={user.stats.followers}
+          views={user.stats.views}
+          likes={user.stats.likes}
+        />
+        <Statistics title="Upload stats" stats={data} />;
+        {/* <Statistics stats={data} />; */}
+        <FriendList friends={friends} />
+      </Container>
     </div>
   );
 }
